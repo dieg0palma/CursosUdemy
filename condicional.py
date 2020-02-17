429186db4988b1f1737ed7d87455a3a267e11782
Diego <33639676+dieg0palma@users.noreply.github.com>
2020-02-17 18:57:10 +0000

    Python codes
    Alguns dos arquivos criados durante o curso de Python

---
+++ condicional.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+x = 13
+y = 12
+
+if x == y: #condicional if
+	print("Os números inseridos são iguais.")
+elif x < y: #condicional elif é uma abreviação para a else if
+	print("X é menor que Y")
+elif x > y:
+	print("X é maior que Y")
+else: #condicional else
+	print("Os números são diferentes")
---
+++ inicio.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+#Como exibir informação na tela:
+print("Olá world!");
+
+"""
+Comentarios
+de
+linha
+multiplas
+"""
+
+#Adição 
+print(2+2);
+
+#Subtração
+print(2-1);
+
+#Multiplicação
+print(2*2);
+
+#Divisão
+print(4/2);
+
+#Exponenciação
+print(2 ** 3);
+
+#Módulo (o resto da divisão)
+print(10 % 3);
+
+var1 = 1; 	#variavél int
+var2 = 1.1; #variável float
+var3 = "a"; #variável string
+var4 = True;
+
+print(var1);
+print(var2);
+print(var3);
+print(var4);
+
+x = 2;
+y = 3;
+
+if x < y:
+	print("2 é menor");
+
+if y > x:
+	print("3 é maior");
